42765a5b94a2144c7ed44f11c591fca5367f308e
arihanv <arihanvaranasi@gmail.com>
2023-06-10 19:12:45 -0500

    Improved Responsiveness and Deletion Ability

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 "use client"
+
 import { useEffect, useState } from "react"
 import Link from "next/link"
+import { Trash, Trash2 } from "lucide-react"
+
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -38,18 +41,23 @@ export default function IndexPage() {
     setHoveredIndex(index)
   }
 
-  const handleMouseLeave = (index: number) => {
+  const handleMouseLeave = () => {
+    setHoveredIndex(-1)
+  }
+
+  const handleDelete = (index: number) => {
+    setSentences((prev: any) => prev.filter((_: any, i: number) => i !== index))
     setHoveredIndex(-1)
   }
 
   return (
     <section className="grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="mx-auto px-3 w-full flex max-w-[980px] flex-col gap-3">
+      <div className="mx-auto px-3 w-full flex max-w-[980px] flex-col gap-3 overflow-hidden">
         <div className="flex flex-col gap-1 border-b dark:border-gray-800 border-gray-200 py-1">
           <div className="tracking-tight font-bold text-xl">Prompt</div>
           <Input
             type="email"
-            placeholder="Email"
+            placeholder="Write your heart out..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
             onKeyDown={(e) => {
@@ -65,30 +73,39 @@ export default function IndexPage() {
           />
         </div>
         <div className="flex flex-col gap-2">
-          {sentences.map((s: any, i: number) => (
-            <div
-              className="flex gap-2"
-              key={i}
-              onMouseEnter={() => handleMouseEnter(i)}
-              onMouseLeave={() => handleMouseLeave(i)}
-            >
-              <div
-                className={`hover:dark:bg-gray-900 hover:bg-gray-100  h-10 rounded-md px-3 w-full py-2  ${
-                  Math.floor(((currentTime as any) - s.timestamp) / 1000) < 0.25
-                    ? ""
-                    : "blur-[2px] fade-in"
-                }`}
-              >
-                {s.text}
-              </div>
-              {hoveredIndex === i && (
-                <span className="text-xs text-gray-400 ml-2 flex flex-col items-center justify-center whitespace-pre">
-                  {getTimeElapsed(s.timestamp)}
-                  {/* {s.timestamp.toLocaleTimeString()} */}
-                </span>
-              )}
-            </div>
-          ))}
+          {sentences.length !== 0 && (
+            <>
+              {sentences.map((s: any, i: number) => (
+                <div
+                  key={i}
+                  onMouseEnter={() => handleMouseEnter(i)}
+                  onMouseLeave={() => handleMouseLeave()}
+                  className={`hover:dark:bg-gray-900 hover:bg-gray-100 rounded-md px-3 w-full py-2 gap-1 flex flex-wrap-reverse justify-between ${
+                    Math.floor(((currentTime as any) - s.timestamp) / 1000) <
+                    0.25
+                      ? ""
+                      : "blur-[2px] fade-in"
+                  }`}
+                >
+                  <span className="whitespace-normal break-words overflow-hidden">
+                    {s.text}
+                  </span>
+                  {hoveredIndex === i && (
+                    <span className="text-xs text-gray-400 flex items-center gap-2 justify-center whitespace-pre">
+                      {getTimeElapsed(s.timestamp)}
+                      <button
+                        className="text-gray-400"
+                        onClick={() => handleDelete(i)}
+                      >
+                        <Trash size={17} />
+                      </button>
+                      {/* {s.timestamp.toLocaleTimeString()} */}
+                    </span>
+                  )}
+                </div>
+              ))}
+            </>
+          )}
         </div>
       </div>
     </section>
--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,12 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Zenset",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "A mental health platform that allows you to write your thoughts and feelings in a safe and secure environment.",
   mainNav: [
     {
-      title: "Home",
+      title: "Write",
       href: "/",
     },
   ],
